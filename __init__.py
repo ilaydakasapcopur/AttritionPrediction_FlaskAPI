f906d9d24da4f60830a92c06849c62fb85e6b107
ilaydakasapcopur <153400251+ilaydakasapcopur@users.noreply.github.com>
2024-01-24 13:26:17 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,93 +1,25 @@
-##############################################################################
-#
-# Copyright (c) 2001, 2002 Zope Foundation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
-"""Interfaces
+from __future__ import annotations
 
-This package implements the Python "scarecrow" proposal.
+import typing as t
 
-The package exports two objects, `Interface` and `Attribute` directly. It also
-exports several helper methods. Interface is used to create an interface with
-a class statement, as in:
-
-  class IMyInterface(Interface):
-    '''Interface documentation
-    '''
-
-    def meth(arg1, arg2):
-        '''Documentation for meth
-        '''
-
-    # Note that there is no self argument
-
-To find out what you can do with interfaces, see the interface
-interface, `IInterface` in the `interfaces` module.
-
-The package has several public modules:
-
-  o `declarations` provides utilities to declare interfaces on objects. It
-    also provides a wide range of helpful utilities that aid in managing
-    declared interfaces. Most of its public names are however imported here.
-
-  o `document` has a utility for documenting an interface as structured text.
-
-  o `exceptions` has the interface-defined exceptions
-
-  o `interfaces` contains a list of all public interfaces for this package.
-
-  o `verify` has utilities for verifying implementations of interfaces.
-
-See the module doc strings for more information.
-"""
-__docformat__ = 'restructuredtext'
-# pylint:disable=wrong-import-position,unused-import
-from zope.interface.interface import Interface
-from zope.interface.interface import _wire
-
-# Need to actually get the interface elements to implement the right interfaces
-_wire()
-del _wire
-
-from zope.interface.declarations import Declaration
-from zope.interface.declarations import alsoProvides
-from zope.interface.declarations import classImplements
-from zope.interface.declarations import classImplementsFirst
-from zope.interface.declarations import classImplementsOnly
-from zope.interface.declarations import directlyProvidedBy
-from zope.interface.declarations import directlyProvides
-from zope.interface.declarations import implementedBy
-from zope.interface.declarations import implementer
-from zope.interface.declarations import implementer_only
-from zope.interface.declarations import moduleProvides
-from zope.interface.declarations import named
-from zope.interface.declarations import noLongerProvides
-from zope.interface.declarations import providedBy
-from zope.interface.declarations import provider
-
-from zope.interface.exceptions import Invalid
-
-from zope.interface.interface import Attribute
-from zope.interface.interface import interfacemethod
-from zope.interface.interface import invariant
-from zope.interface.interface import taggedValue
-
-# The following are to make spec pickles cleaner
-from zope.interface.declarations import Provides
+from .serving import run_simple as run_simple
+from .test import Client as Client
+from .wrappers import Request as Request
+from .wrappers import Response as Response
 
 
-from zope.interface.interfaces import IInterfaceDeclaration
+def __getattr__(name: str) -> t.Any:
+    if name == "__version__":
+        import importlib.metadata
+        import warnings
 
-moduleProvides(IInterfaceDeclaration)
+        warnings.warn(
+            "The '__version__' attribute is deprecated and will be removed in"
+            " Werkzeug 3.1. Use feature detection or"
+            " 'importlib.metadata.version(\"werkzeug\")' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return importlib.metadata.version("werkzeug")
 
-__all__ = ('Interface', 'Attribute') + tuple(IInterfaceDeclaration)
-
-assert all(k in globals() for k in __all__)
+    raise AttributeError(name)
